349112a4a4610cb858ecd588449228c4689852ec
FriwiDev <bblosaurus@web.de>
2020-01-01 20:05:23 +0100

    Add project meta information

--- src/main/java/me/friwi/tello4j/api/drone/TelloDrone.java
+++ src/main/java/me/friwi/tello4j/api/drone/TelloDrone.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2020 Fritz Windisch
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package me.friwi.tello4j.api.drone;
 
 import me.friwi.tello4j.api.exception.*;
 import me.friwi.tello4j.api.state.StateListener;
 import me.friwi.tello4j.api.state.TelloDroneState;
 import me.friwi.tello4j.api.video.TelloVideoExportType;
+import me.friwi.tello4j.api.video.TelloVideoFrame;
 import me.friwi.tello4j.api.video.VideoListener;
 import me.friwi.tello4j.api.world.FlipDirection;
 import me.friwi.tello4j.api.world.MovementDirection;
